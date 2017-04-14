e9b22e559a0db66e20e253055b07a8a34ba98052
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-04-14 12:03:58 +0900

    create master data manager

---
+++ Assets/Scripts/MasterDataManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterDataManager : MonoBehaviour {
+
+	void Start () {
+		TextAsset characterCSV = Resources.Load("CSV/CharacterCSV") as TextAsset;
+	}
+
+	void Update () {
+		
+	}
+}
