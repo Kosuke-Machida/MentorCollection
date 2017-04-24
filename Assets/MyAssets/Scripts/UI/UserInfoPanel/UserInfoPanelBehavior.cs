ce095b683a6ca04fe20360b0b9ea1d0219eb5c96
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-04-24 23:44:54 +0900

    create tap produce function

--- Assets/MyAssets/Scripts/UI/UserInfoPanel/UserInfoPanelBehavior.cs
+++ Assets/MyAssets/Scripts/UI/UserInfoPanel/UserInfoPanelBehavior.cs
@@ -10,6 +10,11 @@ public class UserInfoPanelBehavior : MonoBehaviour {
 	[SerializeField] Text myProductivity;
 	private GameObject[] userInfoPanels;
 
+	void Start ()
+	{
+		StartCoroutine ("FlashText");
+	}
+
 	void Update ()
 	{
 		//panel内のテキストを更新する
@@ -19,10 +24,27 @@ public class UserInfoPanelBehavior : MonoBehaviour {
 	public void SetValues () //panel内のテキストを更新するメソッド
 	{
 		myMoneyText.text = "所持金 : " + string.Format("¥{0:#,0}", PlayerManager.instance.MyMoney);
-		productivityBySecondText.text = "+" + string.Format("{0:#,0}", PlayerManager.instance.MyProductivity / 2);
+		productivityBySecondText.text = "+" + string.Format("{0:#,0}", PlayerManager.instance.MyProductivity/2);
 		workersCountText.text = "従業員 : " + PlayerManager.instance.MyMentors.Count.ToString() +"人";
 		myProductivity.text = "生産性 : " + string.Format("¥{0:#,0}", PlayerManager.instance.MyProductivity);
 	}
+
+	private IEnumerator FlashText ()
+	{
+		while (true)
+		{
+			float alpha = productivityBySecondText.GetComponent<CanvasRenderer>().GetAlpha();
+			if (alpha == 1.0f)
+			{
+				productivityBySecondText.GetComponent<CanvasRenderer>().SetAlpha(0.0f);
+			}
+			else
+			{
+				productivityBySecondText.GetComponent<CanvasRenderer>().SetAlpha(1.0f);
+			}
+			yield return new WaitForSeconds (0.5f);
+		}
+    }
 
 
 }
